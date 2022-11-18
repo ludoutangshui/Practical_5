593f0f3ed2f35bd847f0563e4c6819168e910809
awa <ludoutangshui>
2022-11-18 16:25:00 +0800

    qwq

---
+++ qwq.py
@@ -0,0 +1,11 @@
+def main():
+    b = int(input("b: "))
+    h = int(input("h: "))
+    qwq = areaParellogram(b, h)
+    print(qwq)
+
+def areaParellogram(b, h):
+    awa = b * h
+    return awa
+
+main()
